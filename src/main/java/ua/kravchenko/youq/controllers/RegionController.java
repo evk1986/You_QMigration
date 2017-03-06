e9036d943a0afdaf0b4cbfc17b758640af12428e
EGORSST\Egor <evksst@gmail.com>
2017-03-06 18:06:24 +0200

    fix cities

--- src/main/java/ua/kravchenko/youq/controllers/RegionController.java
+++ src/main/java/ua/kravchenko/youq/controllers/RegionController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ua.kravchenko.youq.entity.Country;
@@ -12,7 +13,6 @@ import ua.kravchenko.youq.entity.Region;
 import ua.kravchenko.youq.services.CountryService;
 import ua.kravchenko.youq.services.RegionService;
 
-import javax.websocket.server.PathParam;
 import java.util.Date;
 import java.util.List;
 
@@ -49,12 +49,33 @@ public class RegionController {
                                           Region region, BindingResult bindingResult,
                                   Model model) {
         Region regionModel = null;
-        if (regionService.findOneById(region.getId()) != null) {
-            regionModel = regionService.findOneById(region.getId());
-        } else {
-            regionModel = this.convertingModel(region);
-        }
-       /* Region regionModel = regionService.findOneById(region.getId());*/
+        regionModel = this.convertingModel(region);
+        regionService.save(regionModel);
+        System.out.println(regionModel);
+        List<Region> regions = regionService.findAll();
+        model.addAttribute("regions", regions);
+        return "redirect:/region/viewall";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editRegionView(@PathVariable(value = "id") Long id, Model model) {
+        Region region = regionService.findOneById(id);
+        System.out.println("GET region" + region.toString());
+        List<Country> countries = countryService.findAll();
+        model.addAttribute("region", region);
+        model.addAttribute("countrys", countries);
+        return "region_edit";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
+    public String updateRegion(@PathVariable(value = "id") Long id,
+                               @ModelAttribute(value = "region") Region region,
+                               BindingResult bindingResult,
+                               Model model) {
+        Region regionModel = regionService.findOneById(id);
+        System.out.println("upcoming from model = " + region.toString());
+        System.out.println("upcoming from DB = " + regionModel.toString());
+
         if (!regionModel.getNameRu().equals(region.getNameRu())) {
             regionModel.setNameRu(region.getNameRu());
         }
@@ -68,28 +89,31 @@ public class RegionController {
         System.out.println(regionModel);
         List<Region> regions = regionService.findAll();
         model.addAttribute("regions", regions);
-        return "redirect:/region/viewall";
+        return "region_view_all";
 
     }
 
-    @RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public String editRegionView(@PathParam(value = "id") Long id, Model model) {
-        Region region = regionService.findOneById(id);
-        List<Country> countries = countryService.findAll();
-        model.addAttribute("region", region);
-        model.addAttribute("countrys", countries);
-        return "region_edit";
-    }
 
-    /*@RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public String updateRegion(@ModelAttribute(value = "region") Region region, Model model) {
-        Region regionModel = this.convertingModel(region);
-        regionService.save(regionModel);
-        List<Region> regions = regionService.findAll();
-        model.addAttribute("regions", regions);
-        return "region_edit";
-    }*/
-
+    /* System.out.println("upcoming from model = " + region.toString());
+        System.out.println("upcoming from DB = " + regionService.findOneById(region.getId()).toString());
+        if (regionService.findOneById(region.getId()) != null) {
+            regionModel = regionService.findOneById(region.getId());
+            System.out.println("region MOdel = " + regionModel.toString());
+                if (!regionModel.getNameRu().equals(region.getNameRu())) {
+                    regionModel.setNameRu(region.getNameRu());
+                }
+                if (!regionModel.getNameEn().equals(region.getNameEn())) {
+                    regionModel.setNameEn(region.getNameEn());
+                }
+                if (!regionModel.getCountry().equals(region.getCountry())) {
+                    regionModel.setCountry(region.getCountry());
+                }
+            regionService.save(regionModel);
+            System.out.println(regionModel);
+            List<Region> regions = regionService.findAll();
+            model.addAttribute("regions", regions);
+            return "redirect:/region/viewall";
+        } */
     private Region convertingModel(Region region) {
         Region model = new Region();
         model.setNameEn(region.getNameEn());
